2cbb204faf700c05f56701d68c68401d665a01fa
Youness <elkhatabi.youness@gmail.com>
2020-11-13 18:36:54 +0100

    update

--- developer.py
+++ developer.py
@@ -7,10 +7,3 @@ class Developer(Employee):
         super().__init__(fname, lname, pay)
         #Employee.__init__(self, fname, lname, pay)
         self.progr_lang = progr_lang
-
-
-dev_1 = Developer('Corey', 'Schafer', 50000, 'Python')
-dev_2 = Developer('James', 'hedy', 60000, 'Java')
-
-print(dev_1.email)
-print(dev_2.progr_lang)
