9691f2976ab4ef19239da94c327cbb7bc8b99517
Youness <elkhatabi.youness@gmail.com>
2020-11-13 13:25:12 +0100

    add a class attribute for counting employees

--- employee.py
+++ employee.py
@@ -11,19 +11,11 @@ class Employee:
         self.lname = lname
         self.pay = pay
         self.email = fname + '.' + lname + '@mycompany.com'
-        self.num_of_emps += 1
+        
+        Employee.num_of_emps += 1
 
     def fullname(self):
         return '{} {}'.format(self.fname, self.lname)
 
     def apply_raise(self):
         self.pay = int(self.pay * self.raise_amount)   
-
-
-emp_1 = Employee('Corey', 'Schafer', 50000)
-#Employee.fullname(emp_1) <=> print(emp_1.fullname())
-emp_1.apply_raise()
-print(emp_1.pay)
-print(emp_1.raise_amount)
-print(Employee.raise_amount)
-print(emp_1.__dict__)
